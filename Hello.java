714ca7b905c92bbff2125c922b563e399cf0d169
Chaoli Zhang <c.jack.zhang@gmail.com>
2020-12-10 17:12:14 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // System.out.println("What is your name?");
+
+        // Scanner scanner = new Scanner(System.in);
+        // String name = scanner.nextLine();
+
+        // System.out.println("Hello " + name);
+
+        // String string1 = new String("string1ss ");
+        // String string2 = new String("string2ss ");
+
+        // String string3 = string1 + string2;
+
+        // System.out.println(string3);
+        //
+
+        int x = 5;
+        Integer y = 5;
+
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+
+}
